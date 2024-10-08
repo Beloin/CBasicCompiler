3dd0e1ba3d57e223103c0ab62e9f8f91002a7dac
Beloin Sena <beloin.rodrigues@gmail.com>
2024-10-07 21:11:42 -0300

    WIP - Adding launch.nvim settings

--- cmd/main.c
+++ cmd/main.c
@@ -34,6 +34,12 @@ static void init() {
 int main(int argc, char *argv[]) {
   struct ASTnode *ast;
   Infile = fopen(argv[1], "r");
+
+  char *c = getenv("ENV_VAR");
+  if (c) {
+    printf("ENV_VAR=%s\n", c);
+  }
+
   if (argc != 2) {
     fprintf(stderr, "Not given any args\n");
     exit(1);
